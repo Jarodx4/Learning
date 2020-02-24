45b91f0da4ab3009c5eda64c78ed3df9c9673d8c
Jarodx4 <60792380+Jarodx4@users.noreply.github.com>
2020-02-24 00:39:24 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,10 +1,10 @@
 # This program says hello and asks for your name and age
 print('Hello!')
 print('What is your name?') 
-myName = input()
+myName = input().strip()
 print('It is good to meet you, ' + myName)
 print('The length of your name is:')
 print(len(myName))
 print('What is your age?') 
-myAge = input()
+myAge = input().strip()
 print ('You will be ' +str(int(myAge) + 1) + ' in a year.')
