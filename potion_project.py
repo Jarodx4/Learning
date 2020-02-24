1c961bbb039443a56eaad15d0b068cd746de5c61
Jarodx4 <60792380+Jarodx4@users.noreply.github.com>
2020-02-24 14:40:02 -0500

    Update potion_project.py

--- potion_project.py
+++ potion_project.py
@@ -1,13 +1,17 @@
 # using random module for health potion
+
 import random
 
 # choose your difficulty
+
 print("Which difficulty would you like? [Easy, Medium, Hard]")
 
 # first variable
+
 difficulty1 = input('Difficulty: ').strip()
 
 # if statement that exits if difficulty does not exist
+
 if 'Easy' in difficulty1:
     print('Potions will heal 25-50 health')
 
@@ -21,14 +25,17 @@ else:
     exit()
 
 # assigning value to 'health' variable
+
 health = 50
 
 # the higher the difficulty the less health the potion will give
 # assigning a list to a variable for this
+
 difficulty2 = [1,2,3]
 
 # another if statement referring back to the first variable to determine output
 # converting all output to integers as we don't want floats in the new variable
+
 if 'Easy' in difficulty1:
     potion_health = random.randint(25,50) / int(difficulty2[0])
     print('This time you will gain...')
@@ -48,7 +55,9 @@ else:
     exit()
 
 # assigning 'health' variable a new value by adding it to our new variable 'potion_health'
+
 health = health + potion_health
 
 # displays health depending on difficulty chosen
+
 print('Your total health is now', int(health))
